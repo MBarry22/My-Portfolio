3e05fc814f75934b112d7eedcd2ce62cf7b519e9
Mason Barry <mbarry22@my.bcit.ca>
2023-02-18 12:40:39 -0800

    Polished Layout and Design. Need to Add Services

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "MASON";
+const meta = {
+    title: "Mason Porter",
+    description: "I'm Mason Porter, a web developer and designer from Canada.",
+};
+
+const introdata = {
+    title: "I'm Mason Porter",
+    animated: {
+        first: "Jr. Software Developer",
+        second: "Full Stack Development",
+        third: "BCIT Alumni 2023",
+    },
+    description: "Quote Of The Day -> What if it turns out better than you could ever imagine?",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "As a BCIT Alumni with a focus on Software Systems Development and Full Stack Development, I possess a strong technical background and a passion for creating projects. My curiosity about how things work fuels my persistence and motivation, driving me to explore complex systems and push the boundaries of what's possible. When I'm not working on software projects, I love spending time outdoors, exploring the natural beauty of Vancouver Island through hiking, caving, and mine exploration. These experiences help me maintain a healthy work-life balance and inspire me to create innovative solutions to technical challenges.",
+};
+const worktimeline = [{
+        jobtitle: "Software Systems Development",
+        where: "BCIT, Vancouver, BC",
+        date: "2022",
+    },
+    {
+        jobtitle: "Fundementals of Python Programming",
+        where: "Vancouver, BC",
+        date: "2021",
+    },
+    {
+        jobtitle: "Self Taught Programming",
+        where: "Nanaimo, BC",
+        date: "2020",
+    },
+];
+
+const skills = [{
+        name: "React Js",
+        value: 85,
+    },
+    {
+        name: "Node Js",
+        value: 85,
+    },
+    {
+        name: "C#",
+        value: 75,
+    },
+    {
+        name: ".Net",
+        value: 75,
+    },
+    {
+        name: "SQL",
+        value: 75,
+    },
+    {
+        name: "Sass",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/MBarry22",
+    facebook: "https://www.facebook.com/mason.porter.775",
+    linkedin: "https://www.linkedin.com/in/mason-porter-b3561b1b5/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
