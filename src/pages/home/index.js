0ebebadebd3e8a631c517442840d39306c8e6bc7
Mason Barry <mbarry22@my.bcit.ca>
2023-04-18 12:27:19 -0700

    test production

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -37,11 +37,11 @@ export const Home = () => {
                     }}
                   />
                 </h1>
-                <p className="mb-1x">{introdata.description}</p>
+                <p className="mb-1x"> {introdata.description}</p>
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
                     <div id="button_p" className="ac_btn btn ">
-                      My Portfolio
+                      My Portfolio | Test Production
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
